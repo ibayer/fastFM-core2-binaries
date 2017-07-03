e23b7d71b2ad6524384eefe0241f3fb60561abd9
Immanuel Bayer <immanuel.bayer@gmail.com>
2017-07-03 16:53:57 +0000

    add missing static libs

--- collect.sh
+++ collect.sh
@@ -2,3 +2,5 @@ cp /root/.hunter/_Base/033a6ff/14d0f80/75730c8/Insta
 ll/lib/libglog.a .
 cp ~/fastFM2/_builds/fastFM/libfastFMd.a .
 cp ~/fastFM2/fastFM/fastfm.h .
+cp ~/musl-cross-make/output/x86_64-linux-musl/lib/libc.a .
+cp ~/musl-cross-make/output/x86_64-linux-musl/lib/libpthread.a .
